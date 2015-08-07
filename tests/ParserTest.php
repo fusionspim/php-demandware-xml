d52fa910513e126e70e8755a10550ff7e89a686d
Wes Hooper <wes@fusionspim.com>
2015-08-07 16:36:30 +0100

    Comment tweaks

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -3,8 +3,8 @@ namespace DemandwareXml\Test;
 
 use \DemandwareXml\Parser;
 
-// note: don't care about/use variants for now
-// to regen fixtures: file_put_contents($path . 'categories.json', json_encode($parser->categories(), JSON_PRETTY_PRINT) . PHP_EOL);
+// note: don't need to parse variants, so no test for those!
+// rebuild fixtures: file_put_contents(__DIR__ . '/fixtures/categories.json', json_encode($parser->categories(), JSON_PRETTY_PRINT) . PHP_EOL);
 class ParserTest extends AbstractTest
 {
     public function testAssignmentsParser()
