3a087363f150f5b58ce4085b2e5c7ded115f781d
Jamie York <ziadoz@gmail.com>
2015-08-10 11:00:26 +0100

    Fix error handler params

--- src/Xml.php
+++ src/Xml.php
@@ -39,8 +39,8 @@ class Xml
 
         libxml_use_internal_errors(true);
 
-        set_error_handler(function ($severity, $message, $file, $line) {
-            throw new XmlException($message, $severity, $severity, $file, $line);
+        set_error_handler(function ($number, $message, $file, $line, $context) {
+            throw new XmlException($message, $number);
         });
 
         // possibly more efficient to pass a $dom object rather than save/reload, but cleaner to assume already saved
