4faf754fea3dffa5f8131538dcf7e1bacb918f1a
Jamie York <ziadoz@gmail.com>
2016-01-20 10:33:59 +0000

    Correct regular expression

--- src/Xml.php
+++ src/Xml.php
@@ -35,7 +35,7 @@ class Xml
      */
     public static function sanitise($string)
     {
-        return preg_replace('/[^\x{0009}\x{000A}\x{000D}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]+/u', ' ', $string);
+        return preg_replace('/[^\x{0009}\x{000A}\x{000D}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]/u', ' ', $string);
     }
 
     /**
