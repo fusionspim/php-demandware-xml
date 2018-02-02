9b43192b5b4004a72df7e773edad15a4b10840a7
Jamie York <ziadoz@gmail.com>
2018-02-02 10:39:47 +0000

    Add node parser interface docs.

--- src/Parser/NodeParserInterface.php
+++ src/Parser/NodeParserInterface.php
@@ -5,7 +5,12 @@ use XMLReader;
 
 interface NodeParserInterface
 {
+    // Give the parser the current node.
     public function __construct(XMLReader $reader);
+
+    // Determine if the give node is a match for parsing.
     public function isMatch(): bool;
+
+    // Parse the given node into an array.
     public function parse(): array;
 }
