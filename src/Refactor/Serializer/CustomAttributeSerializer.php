de09906bda9bb735cf25ea32495b748b3f508f4b
Jamie <ziadoz@gmail.com>
2018-09-18 12:01:19 +0100

    CustomAttributes are allowed to be empty (for notifying they've been removed).

--- src/Refactor/Serializer/CustomAttributeSerializer.php
+++ src/Refactor/Serializer/CustomAttributeSerializer.php
@@ -27,26 +27,36 @@ class CustomAttributeSerializer implements SerializerInterface
 
     private function serializeSingle()
     {
-        $this->writer->ifNotEmpty()->writeElementWithAttributes('custom-attribute', Formatter::fromType($this->customAttribute->value), [
-            'attribute-id' => $this->customAttribute->id
-        ]);
+        $value = Formatter::fromType($this->customAttribute->value);
+
+        if (! Formatter::isEmpty($value)) {
+            $this->writer->writeElementWithAttributes('custom-attribute', Formatter::fromType($this->customAttribute->value), [
+                'attribute-id' => $this->customAttribute->id
+            ]);
+        } else {
+            $this->writer->writeEmptyElementWithAttributes('custom-attribute', [
+                'attribute-id' => $this->customAttribute->id
+            ]);
+        }
     }
 
     private function serializeMultiple()
     {
         $values = Formatter::filterEmpty($this->customAttribute->value);
 
-        if (count($values) === 0) {
-            return;
+        if (count($values) > 0) {
+            $this->writer->startElement('custom-attribute');
+            $this->writer->writeAttribute('attribute-id', $this->customAttribute->id);
+
+            foreach ($values as $value) {
+                $this->writer->writeElement('value', Formatter::fromType($value));
+            }
+
+            $this->writer->endElement();
+        } else {
+            $this->writer->writeEmptyElementWithAttributes('custom-attribute', [
+                'attribute-id' => $this->customAttribute->id
+            ]);
         }
-
-        $this->writer->startElement('custom-attribute');
-        $this->writer->writeAttribute('attribute-id', $this->customAttribute->id);
-
-        foreach ($values as $value) {
-            $this->writer->writeElement('value', Formatter::fromType($value));
-        }
-
-        $this->writer->endElement();
     }
 }
