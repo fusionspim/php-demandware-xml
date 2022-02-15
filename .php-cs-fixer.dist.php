0be37f041911c7071f6cc356922bcb062073be05
Jamie <ziadoz@gmail.com>
2022-02-15 12:15:09 +0000

    Remove Rector

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,10 +1,5 @@
 <?php
-$config = FusionsPim\PhpCsFixer\Factory::fromDefaults([
+return FusionsPim\PhpCsFixer\Factory::fromDefaults([
     'date_time_immutable' => false, // Needed for tests
     'group_import'        => false, // Currently broken for root classes
 ]);
-
-return $config->setFinder(
-    $config->getFinder()
-        ->notName('rector.php')
-);
