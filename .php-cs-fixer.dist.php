373e720e487293c6cd3df6c9e8d9d3ea29a5aca3
Liam <liamk15@gmail.com>
2022-11-02 13:41:56 +0000

    remove php-cs config

--- .php-cs-fixer.dist.php
+++
@@ -1,5 +0,0 @@
-<?php
-return FusionsPim\PhpCsFixer\Factory::fromDefaults([
-    'date_time_immutable' => false, // Needed for tests
-    'group_import'        => false, // Currently broken for root classes
-]);
