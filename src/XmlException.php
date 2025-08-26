b1059c9aa9041983fec5bd474a2334a32b6cd1ca
Katie Smith <108671517+ktsm-th@users.noreply.github.com>
2025-08-26 12:08:42 +0100

    Update XmlException.php

--- src/XmlException.php
+++ src/XmlException.php
@@ -9,17 +9,26 @@ use Exception;
  */
 class XmlException extends Exception
 {
-    protected ?string $url = null;
+    protected ?string $invalidCharUrl = null;
 
-    public function __construct($message = '', $code = 0, ?\Throwable $previous = null, ?string $url = null)
+    public function __construct($message = '', $code = 0, ?\Throwable $previous = null)
     {
-        $this->url = $url;
+        $this->invalidCharUrl = $this->parseInvalidChar($message);
 
         parent::__construct($message, $code, $previous);
     }
 
-    public function getUrl(): ?string
+    public function getInvalidCharUrl(): ?string
     {
-        return $this->url;
+        return $this->invalidCharUrl;
+    }
+
+    protected function parseInvalidChar(?string $message): ?string
+    {
+        if (preg_match('/^Fatal: PCDATA invalid Char value (\d+)/', $message, $matches)) {
+            return sprintf('https://www.fileformat.info/info/unicode/char/%d/index.htm', $matches[1]);
+        }
+
+        return null;
     }
 }
